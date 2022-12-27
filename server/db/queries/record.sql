cdcacd95eecd780772b3357505a2b9b8c10013f2
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-27 19:35:06 +0100

    🔨 gen sqlc db calls !

---
+++ server/db/queries/record.sql
@@ -0,0 +1,12 @@
+
+-- name: CreateRecord :one
+INSERT INTO records
+(id, unit, value, type)
+VALUES ($1, $2, $3, $4) RETURNING *;
+
+-- name: GetRecords :many
+SELECT * FROM records
+ORDER BY created_at;
+
+
+
