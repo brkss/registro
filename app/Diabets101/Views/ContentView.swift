aa36880ebfa04ec2f838cedefa12ce77f02ecfcf
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-29 18:47:23 +0100

    🚨 set notiofication (permession and testing)

--- app/Diabets101/Views/ContentView.swift
+++ app/Diabets101/Views/ContentView.swift
@@ -7,9 +7,27 @@
 
 import SwiftUI
 import UIKit
+import UserNotifications
 
 struct ContentView: View {
-    
+    init(){
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]){success, error in
+            if success {
+                print("Permission all set !")
+            }else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Measure Your Blood Sugar 🩸"
+        content.subtitle = "it been 2 hours since your last record"
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let req = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(req)
+    }
     var body: some View {
         TabView{
             RecordsList()
