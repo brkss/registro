be21f2885d9d16e4c0087af8dea0dc71f83c0e2c
Brahim Berkasse <berkassebrahim@gmail.com>
2022-12-30 20:37:38 +0100

    🔨 set gitignore !

--- server/db/migration/000001_init.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE "records" (
-	"id" varchar PRIMARY KEY NOT NULL,
-	"unit" varchar NOT NULL,
-	"value" integer NOT NULL,
-	"type" varchar NOT NULL,
-	"created_at" timestamptz NOT NULL DEFAULT (now())
-);
